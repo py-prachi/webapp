a9cea877d329cb4fb6976afc3127c4ca468a984e
py-prachi <prachi.249@gmail.com>
2023-09-22 14:03:36 +0530

    env variables set for db config

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,19 +2,20 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "./entity/User";
 //import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+require("dotenv").config();
 
 const AppDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "test",
-  password: "password",
-  database: "webapp",
-  synchronize: false,
-  logging: false,
-  entities: [User],
-  //migrations: [__dirname + '/migrations/*.ts'],
-  subscribers: [],
+    type: (process.env.DB_TYPE || "postgres") as "postgres",
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || "5432"),  
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    synchronize: process.env.DB_SYNCHRONIZE === "true",  
+    logging: process.env.DB_LOGGING === "true",  
+    entities: [User],
+    //migrations: [__dirname + '/migrations/*.ts'],
+    subscribers: [],
 });
 
 export { AppDataSource };
